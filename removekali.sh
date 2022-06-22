13da6bf7e260e68c62c2460e070ad3168e20ca3e
winux <78425126+Pedro1234-code@users.noreply.github.com>
2022-06-22 11:50:43 -0300

    Update removekali.sh

--- removekali.sh
+++ removekali.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+touch demo.txt
+if zenity --question --title="Remove Kali Linux" --text="Are you sure you want to remove Kali Linux chroot?" --no-wrap
+    then
+    sudo stopkali && sudo rm -rf /usr/bin/stopkali && sudo rm -rf /usr/bin/startkali && sudo rm -rf /kali-fs
+    zenity --info --title="Success" --text="Kali sucefully removed" --no-wrap
+fi
